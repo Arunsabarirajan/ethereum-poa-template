c7cd22a5b0ceced1669bb1e6130601359c510038
Arun Sabarirajan <Arun_Sabarirajan@singaporeair.com.sg>
2021-01-12 20:00:59 +0800

    Move docker image to geth version 1.8.27 stable

--- docker-images/eth-stats-dashboard/updateNode.sh
+++ docker-images/eth-stats-dashboard/updateNode.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.1/install.sh | bash
 
 export NVM_DIR="$HOME/.nvm"
--- docker-images/geth-node/runGeth.sh
+++ docker-images/geth-node/runGeth.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 GENESIS=/home/eth-node/genesis.json
+TMPDIR=/home/eth-node/.tmp
 DATADIR=/home/eth-node/.geth
 RPCPORT=8545
 RPCHOST=0.0.0.0
 GETHPORT=30303
-GETHARGS=
+GETHARGS=$EXTERNAL_ARGS
 BOOTNODE_URL="$BOOTNODE_URL/staticenodes?network=$BOOTNODE_NETWORK"
 BOOTNODES=$(curl --connect-timeout 1 --retry 10  --retry-max-time 10 -f -s $BOOTNODE_URL)
 STATSARGS="--ethstats \"$NODE_NAME:$WS_SECRET@$WS_SERVER\""
@@ -25,6 +26,40 @@ if [ -z "$NODE_NAME" ]; then
   exit 1
 fi
 
+if [ "${NETWORKID}" == "1" ]; then
+    echo "Using Main Net"
+elif [ "${NETWORKID}" == "3" ]; then
+    GETHARGS="${GETHARGS} --testnet"
+elif [ "${NETWORKID}" == "4" ]; then
+    GETHARGS="${GETHARGS} --rinkeby"
+else
+  if [ ! -d "$DATADIR/chaindata" ]; then
+    geth --datadir $DATADIR init $GENESIS
+  fi
+    GETHARGS="${GETHARGS} --nodiscover --networkid $NETWORKID"
+if [ "$SEALER_KEY" ]; then
+    if [  -d "$DATADIR/keystore" ]; then
+      rm -rf $DATADIR/keystore/*
+    fi
+
+    mkdir -p $TMPDIR
+    KEY_FILE="$TMPDIR/private.key"
+    PW_FILE="$TMPDIR/private.pwd"
+    echo "Initialize sealer"
+    echo $SEALER_KEY>$KEY_FILE
+
+    if [ ! -e $PW_FILE ]; then
+      echo $SEALER_PW>$PW_FILE
+      chmod 0600 $PW_FILE
+    fi
+
+    MINER_ADDRESS=$(geth --datadir $DATADIR --password $PW_FILE account import $KEY_FILE | cut -c 11-50)
+    ENABLE_MINER=1
+    rm $KEY_FILE
+    GETHARGS="${GETHARGS} --unlock "0" --password ${PW_FILE} --allow-insecure-unlock"
+  fi
+fi
+
 if [ "$ENABLE_MINER" ]; then
   if [ -z "$MINER_ADDRESS" ]; then
     echo "No MINER_ADDRESS was supplied"
@@ -32,19 +67,16 @@ if [ "$ENABLE_MINER" ]; then
   fi
 
   while [ -z "$BOOTNODES" ]
-  do	
+  do
     BOOTNODES=$(curl --connect-timeout 1 --retry 10  --retry-max-time 10 -f -s $BOOTNODE_URL)
   done
 
-  GETHARGS="--mine --etherbase $MINER_ADDRESS"
+  GETHARGS="$GETHARGS --mine --etherbase $MINER_ADDRESS"
 
   if [ "$MINER_THREADS" ]; then
     GETHARGS="$GETHARGS --minerthreads $MINER_THREADS"
   fi
-else
-  GETHARGS="--fast"
 fi
-
 
 if [ "$BOOTNODES" ]; then
   echo "Adding bootnodes"
@@ -52,19 +84,9 @@ if [ "$BOOTNODES" ]; then
   echo $BOOTNODES > $DATADIR/static-nodes.json
 fi
 
-if [ ! -d "$DATADIR/chaindata" ]; then
-  geth --datadir $DATADIR init $GENESIS
-fi
-
-echo "BOOTNODES"
-echo $BOOTNODES
-echo $GETHARGS
 
 geth --datadir $DATADIR \
         --identity $NODE_NAME \
-        --nodiscover \
         --rpc --rpcport $RPCPORT --rpcaddr $RPCHOST \
-        --networkid $NETWORKID \
         --port $GETHPORT \
         $GETHARGS 2>&1
-
